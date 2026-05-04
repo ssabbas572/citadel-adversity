9bc34476ee3ad19a6e3e7744d0977c46755f0177
Syed Sajid Abass <SSajidAbass@infinityels.com>
2026-05-04 22:50:14 +0500

    my test

---
+++ main.py
@@ -0,0 +1,11 @@
+# sample.py
+
+def greet(name):
+    return f"Hello, {name}!"
+
+def main():
+    name = input("Enter your name: ")
+    print(greet(name))
+
+if __name__ == "__main__":
+    main()
